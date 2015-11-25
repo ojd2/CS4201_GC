59a2ff96566f68a98b04576aedca66cc3ccf59b1
ojd2 <ojd2@st-andrews.ac.uk>
2015-11-25 13:55:16 +0000

    Updated variables

---
+++ README.txt
@@ -0,0 +1,24 @@
+/**
+ * Generational Garbage Collection technique.
+ *
+ *
+ * - C++ GC Methods:
+ *
+ * - Main points of GC: http://www.codeproject.com/Articles/1060/Garbage-Collection-in-NET (generations)
+ * - Using the hboehm Garbage Collector: http://www.hboehm.info/gc/simple_example.html
+ * - Heap : https://gist.github.com/nandor/ef9052412e5ccc301a64
+ *
+ * - JavaScript GC Methods:
+ *
+ * - Mark and Sweep GC: https://gist.github.com/DmitrySoshnikov/4391763
+ * - Reference Counting (ARC) https://gist.github.com/DmitrySoshnikov/4646658
+ * - Stop and Copy GC: https://gist.github.com/DmitrySoshnikov/4736334
+ * - Non-recursive DFS and BFS: https://gist.github.com/DmitrySoshnikov/63f9acfac4651da5d21f
+ * 
+ * - Research Papers:
+ * - Generation Scavenging Algorithm by Ungar: http://www.cs.utexas.edu/users/mckinley/395Tmm-2003/talks/Ungar.pdf
+ * - Incremental, Generational Mostly-Copying GC: http://www.hpl.hp.com/techreports/Compaq-DEC/WRL-91-8.pdf
+ * - A tour of the V8 GC engine: http://jayconrod.com/posts/55/a-tour-of-v8-garbage-collection
+ * - Garbage Collection Algorithms - http://courses.cs.washington.edu/courses/csep521/07wi/prj/rick.pdf
+ *
+ */
--- gc.js
+++ gc.js
@@ -1,27 +1,6 @@
 /**
  * Generational Garbage Collection technique.
- *
- *
- * - C++ GC Methods:
- *
- * - Main points of GC: http://www.codeproject.com/Articles/1060/Garbage-Collection-in-NET (generations)
- * - Using the hboehm Garbage Collector: http://www.hboehm.info/gc/simple_example.html
- * - Heap : https://gist.github.com/nandor/ef9052412e5ccc301a64
- *
- * - JavaScript GC Methods:
- *
- * - Mark and Sweep GC: https://gist.github.com/DmitrySoshnikov/4391763
- * - Reference Counting (ARC) https://gist.github.com/DmitrySoshnikov/4646658
- * - Stop and Copy GC: https://gist.github.com/DmitrySoshnikov/4736334
- * - Non-recursive DFS and BFS: https://gist.github.com/DmitrySoshnikov/63f9acfac4651da5d21f
- * 
- * - Research Papers:
- * - Generation Scavenging Algorithm by Ungar: http://www.cs.utexas.edu/users/mckinley/395Tmm-2003/talks/Ungar.pdf
- * - Incremental, Generational Mostly-Copying GC: http://www.hpl.hp.com/techreports/Compaq-DEC/WRL-91-8.pdf
- * - A tour of the V8 GC engine: http://jayconrod.com/posts/55/a-tour-of-v8-garbage-collection
- * - Garbage Collection Algorithms - http://courses.cs.washington.edu/courses/csep521/07wi/prj/rick.pdf
- *
- */
+**/
 
 // ---------------------------------------------------------------------------
 // I. Overall Goal for our Generational Garbage Collection program.
@@ -36,8 +15,8 @@
 
 // To put the following program into perspective, first the program should use some simple
 // Heap structure, memory, allocation and object referencing functions. 
-// These can then be broken into two generations: 'FROM_BOUND' (old_generation), 
-// 'TO_BOUND' (young_generation). The young generation: Most of the newly created objects 
+// These can then be broken into two generations: 'FROM_SPACE' (old_generation), 
+// 'TO_SPACE' (young_generation). The young generation: Most of the newly created objects 
 // are located here. The from-space: Objects that did not become unreachable / survived 
 // the to-space are copied here. When objects disappear from the to-space: 
 // we say a 'minor GC' has occurred. When objects disappear from the old generation: we say a 
@@ -78,35 +57,34 @@ var heap = Array(20); // heap of size 20
 // for different ages of objects.
 
 // Current from-space (working) space.
-var FROM_BOUND = 0; 
+var FROM_SPACE = 0; 
 // Start of the to-space space.
-var TO_BOUND = 10; 
+var TO_SPACE = 10; 
 // Spaces for the generation objects to be passed after GC.
 var G_0 = {};
 var G_1 = {};
 var G_2 = {};
 
-console.log(FROM_BOUND);
-console.log(TO_BOUND);
+console.log(FROM_SPACE);
+console.log(TO_SPACE);
 
 // Initially the allocation pointer is set to the beginning of 
 // the from-space (to zero).
-var ALLOC_POINTER = FROM_BOUND;
+var POINTER = FROM_SPACE;
 
 // Set up a function which denotes allocation of various objects. 
 // Then assign our objects and store onto the heap.
-// The 'ALLOC_POINTER' returns object(s) back.
+// The 'POINTER' returns object(s) back.
 // We also explicitly keep the address on the object 
 // the heap array index, where the object is allocated.
-// Incrementally this increases the 'ALLOC_POINTER'.
+// Incrementally this increases the 'POINTER'.
+function pointAddress(object) {
+  // Puts 'object' inside 'heap[POINTER]'
+  heap[POINTER] = object;
+  // Set up post-increment operator
+  POINTER++;
 
-function alloc_struct(object) {
-	// Puts 'object' inside 'heap[ALLOC_POINTER]'
-	heap[ALLOC_POINTER] = object;
-	// Set up post-increment operator
-	ALLOC_POINTER++;
-
-	return object;
+  return object;
 }
 
 
@@ -117,12 +95,12 @@ function alloc_struct(object) {
 // Let's add some 'boxed' objects onto the heap.
 // Object 'INT' is allocated at address 0
 // This object is now 'reachable' from the 'root' index:
-var INT = alloc_struct({a: 10});
+var INT = pointAddress({a: 10});
 console.log(INT);
 
 // Object 'STRING' is allocated at address 1
 // This object is now 'reachable' from the 'INT' object:
-var STRING = alloc_struct({b: 'Rocket man'});
+var STRING = pointAddress({b: 'Rocket man'});
 // The allocation uses the address (array index) 
 // where "STRING" is allocated on the heap:
 INT['STRING'] = STRING['address']; 
@@ -131,7 +109,7 @@ console.log(STRING);
 // Object "BOOL" is allocated at address 2
 // The object is now 'reachable' from "INT": 
 // e.g = root -> INT -> BOOL
-var BOOL = alloc_struct({c: true});
+var BOOL = pointAddress({c: true});
 // The allocation uses the address (array index) where "BOOL"
 // is allocated on the heap as follows:
 INT['BOOL'] = BOOL['address']; 
@@ -146,7 +124,7 @@ console.log(INT);
 // The object is 'reachable' from 'string' 
 // (which in turn is reachable from:
 // 'INT': root -> INT -> STRING -> STRING_
-var STRING_ = alloc_struct({d: 'Hello'});
+var STRING_ = pointAddress({d: 'Hello'});
 STRING['STRING_'] = STRING_['address']; 
 
 // But then the 'STRING' object reference is removed from 'INT'.
@@ -162,7 +140,7 @@ console.log(INT);
 
 // Object 'BOOL_' is allocated at address 4 
 // This becomes 'reachable' from the object 'INT'
-var BOOL_ = alloc_struct({e: false});
+var BOOL_ = pointAddress({e: false});
 INT['BOOL_'] = BOOL_['address'];
 // IMPORTANT: the object 'BOOL_' also has back-reference to 'INT':
 BOOL_['INT'] = INT['address'];
@@ -198,33 +176,33 @@ console.log(INT);
 // by forwarding addresses. This will update any other objects and their pointers 
 // incrementally and we can denote markers accordingly by dividing the to-space 
 // into three parts: 
-// 	 Copied & Scanned objects *
-// 	 Just copied objects *
-// 	 And the Free space *
+//   Copied & Scanned objects *
+//   Just copied objects *
+//   And the Free space *
 
 // Our new allocation pointers are set to the boundary of both the to-space and from-space
 // or 'old generation' and 'young generation' (that is, to the middle of our heap array).
 
 // Allocate from the to-space now at GC:
-ALLOC_POINTER = TO_BOUND;
+POINTER = TO_SPACE;
 // And the Scanner pointer is set initially here too:
-var SCANNER_POINTER = ALLOC_POINTER;
+var SCANNER_POINTER = POINTER;
 
 // Now let's set up a function to connect objects to their new locations.
 function copyNewSpace(object) {
-	// Now copy empty {}
-	var newCopiedObject = {};
-	for (var item in object) {
-		if (item != 'address' && item != 'forwardingAddress') {
-      		newCopiedObject[item] = object[item];
-    	}
-	}
-	// Now mark the old object as copied:
-	object['forwardingAddress'] = ALLOC_POINTER;
-	// Put on the heap (which increases the alloc_struct pointer).
-  	alloc_struct(newCopiedObject);
+  // Now copy empty {}
+  var newCopiedObject = {};
+  for (var item in object) {
+    if (item != 'address' && item != 'forwardingAddress') {
+          newCopiedObject[item] = object[item];
+      }
+  }
+  // Now mark the old object as copied:
+  object['forwardingAddress'] = POINTER;
+  // Put on the heap (which increases the pointAddress pointer).
+    pointAddress(newCopiedObject);
 
-	return newCopiedObject;
+  return newCopiedObject;
 }
 
 // Now let's set up a function to check if value is an address:
@@ -252,51 +230,51 @@ function gc_sc() {
   // For now we have only the 'INT' object. During our scanning, we copy all 
   // these sub-objects and mark them as copied too (set the "forwarding address" flag).
 
-	while (SCANNER_POINTER != ALLOC_POINTER) { 
-  		// Get the next object within our Scanner pointer:
-    	var nextObjectScan = heap[SCANNER_POINTER];
-  		// Begin a simple traversal algorithm, checking all its reference properties
-    	for (var p1 in nextObjectScan) if (isAddressPointer(p1, nextObjectScan[p1])) {
+  while (SCANNER_POINTER != POINTER) { 
+      // Get the next object within our Scanner pointer:
+      var nextObjectScan = heap[SCANNER_POINTER];
+      // Begin a simple traversal algorithm, checking all its reference properties
+      for (var p1 in nextObjectScan) if (isAddressPointer(p1, nextObjectScan[p1])) {
       
-     		var address = nextObjectScan[p1]; 
-     		// Get the object to which this reference points to:
-      		var objectAtAddress = heap[address];
+        var address = nextObjectScan[p1]; 
+        // Get the object to which this reference points to:
+          var objectAtAddress = heap[address];
 
-      		// If that object hasn't been copied yet (i.e. has no forwarding address set)
-        	if (!('forwardingAddress' in objectAtAddress)) {
-        	// Then we copy this sub-object as well and mark it specifying 'forwardingAddress'.
-        	var copiedObjectAtAddress = copyNewSpace(objectAtAddress);
-        	// And we also need the pointer value on the scanning object to
-        	// refer to the new location.
-        	nextObjectScan[p1] = copiedObjectAtAddress.address;
-      		} 
-      		// Else, the object which this sub-property refers to, was already copied at
-      		// some previous scan of some other objects (an object can be referred by many
-      		// properties in different objects)
-      		else {
-        		// Then we just update the pointer to the forwarding address
-        		nextObjectScan[p1] = objectAtAddress.forwardingAddress;
-      		}
-    	}
+          // If that object hasn't been copied yet (i.e. has no forwarding address set)
+          if (!('forwardingAddress' in objectAtAddress)) {
+          // Then we copy this sub-object as well and mark it specifying 'forwardingAddress'.
+          var copiedObjectAtAddress = copyNewSpace(objectAtAddress);
+          // And we also need the pointer value on the scanning object to
+          // refer to the new location.
+          nextObjectScan[p1] = copiedObjectAtAddress.address;
+          } 
+          // Else, the object which this sub-property refers to, was already copied at
+          // some previous scan of some other objects (an object can be referred by many
+          // properties in different objects)
+          else {
+            // Then we just update the pointer to the forwarding address
+            nextObjectScan[p1] = objectAtAddress.forwardingAddress;
+          }
+      }
 
-    	// And then we move to the next object to scan (the sub-objects which were copied
-    	// at scanning of their parent object, and which not scanned yet).
-    	SCANNER_POINTER++;
-	}
+      // And then we move to the next object to scan (the sub-objects which were copied
+      // at scanning of their parent object, and which not scanned yet).
+      SCANNER_POINTER++;
+  }
     // Now we can swap from-space and to-space, making the to-space our working
     // runtime memory, and the from-space reserved for GC.
-  	for (var k = FROM_BOUND; k < TO_BOUND; k++) {
-    	// Just clean old space for the debug purpose. In real practice it's not
-    	// necessary, this addresses can be just overridden by later allocations.
-    	delete heap[k];
- 	}
+    for (var k = FROM_SPACE; k < TO_SPACE; k++) {
+      // Just clean old space for the debug purpose. In real practice it's not
+      // necessary, this addresses can be just overridden by later allocations.
+      delete heap[k];
+  }
 
     // Store our five live objects
-  	var store = TO_BOUND;
-  	// Reset = 0 objects
-  	TO_BOUND = FROM_BOUND;
-  	// Reset = 5 objects
-  	FROM_BOUND = store;
+    var storeAll = TO_SPACE;
+    // Reset = 0 objects
+    TO_SPACE = FROM_SPACE;
+    // Reset = 5 objects
+    FROM_SPACE = storeAll;
 
 } // End gc_sc();
 
@@ -315,24 +293,24 @@ function objectToString(object) {
     // typeof 'object' and pushes elements into our empty_string[] array.
     // This is done first:
     if (typeof(object) == "object" && (object.join == undefined)) {
-    	for (item in object) {
-        	empty_string.push(item, " -> ", objectToString(object[item]), "\n");
-    	};
-	}
-	// Next we look for items if it is an array: 
-	else if (typeof(object) == "object" && !(object.join == undefined)) {
-	    for(item in object) {
-	        empty_string.push(objectToString(object[item]), "\n");
-	    }
-	} 
-	// If objects is a function
-	else if (typeof(object) == "function") {
-	    empty_string.push(object.toString(), "\n");
-	} 
-	// For all other other values can be done with JSON.stringify
-	else {
-	    empty_string.push(JSON.stringify(object), "\n");
-	}
+      for (item in object) {
+          empty_string.push(item, " -> ", objectToString(object[item]), "\n");
+      };
+  }
+  // Next we look for items if it is an array: 
+  else if (typeof(object) == "object" && !(object.join == undefined)) {
+      for(item in object) {
+          empty_string.push(objectToString(object[item]), "\n");
+      }
+  } 
+  // If objects is a function
+  else if (typeof(object) == "function") {
+      empty_string.push(object.toString(), "\n");
+  } 
+  // For all other other values can be done with JSON.stringify
+  else {
+      empty_string.push(JSON.stringify(object), "\n");
+  }
     // Return our results and values and join them simply to our empty_string[]
     return empty_string.join("");
 }
@@ -393,16 +371,16 @@ console.log('HEAP AFTER GC_SC:' + '\n\n', objectToString(heap));
 // within each G_0, G_1, G_2 etc.
 
 function generation(object, array) {
-	// Pull elements from heap and push into our array.
-	if (typeof(object) == "object") {
-    	for (item in object) {
-    	   // Segment our heap items.
+  // Pull elements from heap and push into our array.
+  if (typeof(object) == "object") {
+      for (item in object) {
+         // Segment our heap items.
           for (var i = 0; i < item.length;)
-            	// Then for each item push into array
-            	i++;
-        		array[item] = object[item];
-      		}
-    	};
+              // Then for each item push into array
+              i++;
+            array[item] = object[item];
+          }
+      };
     // Return our farray
     return array;
 }
@@ -443,11 +421,11 @@ console.log('REARRANGE HEAP:');
 
 // Object 'STRING' is now allocated at address 1
 // This object is now 'reachable' from the 'root' index:
-var STRING = alloc_struct({f: 'Once Upon A Time'});
+var STRING_1 = pointAddress({f: 'Once Upon A Time'});
 // The allocation uses the address (array index) 
 // where "STRING" is allocated on the heap:
-INT['STRING'] = STRING['address']; 
-console.log(STRING);
+INT['STRING_1'] = STRING_1['address']; 
+console.log(STRING_1);
 
 // But then both STRING & BOOL_ object references are removed from 'INT'.
 delete INT['STRING'];
@@ -456,15 +434,15 @@ delete INT['BOOL_'];
 // Object "STRING_" is allocated at address 2
 // The object is now 'reachable' from "INT": 
 // e.g = root -> INT -> STRING_
-var STRING_ = alloc_struct({g: 'In The West'});
+var STRING_2 = pointAddress({g: 'In The West'});
 // The allocation uses the address (array index) where 'STRING_'
 // is allocated on the heap as follows:
-INT['STRING_'] = STRING_['address']; 
-console.log(STRING_);
+INT['STRING_2'] = STRING_2['address']; 
+console.log(STRING_2);
 
 // Now, let us delete the 'STRING_' reference from 'INT'
 // Now 'STRING_' is 'unreachable' and subsequently becomes a candidate for GC.
-delete INT['STRING_'];
+delete INT['STRING_2'];
 
 // Show results of our heap before our GC_SC() callback.
 console.log('HEAP BEFORE GC_SC:' + '\n\n', objectToString(heap));
@@ -490,24 +468,24 @@ console.log('G_1 RESULTS:' + '\n\n' + objectToString(G_1));
 // Let's add some 'boxed' objects onto the heap.
 // Object 'STRING' is allocated at address 0
 // This object is now 'reachable' from the 'root' index:
-var STRING = alloc_struct({h: 'One last time'});
-console.log(STRING);
+var STRING_3 = pointAddress({h: 'One last time'});
+console.log(STRING_3);
 
 // The allocation uses the address (array index) 
 // where "INT" is allocated on the heap:
-STRING['INT'] = INT['address']; 
+INT['STRING_3'] = INT['address']; 
 console.log(INT);
 
 // Object "STRING_" is allocated at address 3
 // The object is 'reachable' from 'STRING' 
 // (which in turn is reachable from:
 // 'INT': root -> STRING -> INT -> STRING__
-var STRING_ = alloc_struct({k: 'Hello there my friends'});
-INT['STRING'] = STRING_['address']; 
+var STRING_4 = pointAddress({k: 'Hello there my friends'});
+INT['STRING_4'] = INT['address']; 
 
 // Now, let us delete the 'STRING' reference from 'INT'
 // Now 'STRING' is 'unreachable' and subsequently becomes a candidate for GC.
-delete INT['STRING'];
+delete INT['STRING_4'];
 
 // Show results of our heap before our GC_SC() callback.
 console.log('HEAP BEFORE GC_SC:' + '\n\n', objectToString(heap));
@@ -525,3 +503,6 @@ console.log('HEAP AFTER GC_SC:' + '\n\n', objectToString(heap));
 console.log('PUSH HEAP INTO G_2;');
 generation(heap, G_2);
 console.log('G_2 RESULTS:' + '\n\n' + objectToString(G_2));
+
+
+// GIT VERSION HAS BEFORE AMENDS OF STRING_ VALUES ETC..
